9b75484621e737bc3fac5bc561c7302bc9f5bf9b
Shadow17402 <brinar84@gmail.com>
2020-11-12 11:19:21 +0100

    Added Copper Block

--- src/main/java/net/kaindorf/mineterra/block/ModBlocks.java
+++ src/main/java/net/kaindorf/mineterra/block/ModBlocks.java
@@ -2,6 +2,7 @@ package net.kaindorf.mineterra.block;
 
 import net.kaindorf.mineterra.block.pedestal.BlockPedestal;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
@@ -9,11 +10,13 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class ModBlocks {
 
     public static BlockOre oreCopper = new BlockOre("ore_copper","oreCopper");
+    public static BlockBase blockCopper = new BlockBase(Material.IRON,"block_copper");
     public static BlockPedestal pedestal = new BlockPedestal();
 
     public static void register(IForgeRegistry<Block> registry) {
         registry.registerAll(
                 oreCopper,
+                blockCopper,
                 pedestal
         );
         GameRegistry.registerTileEntity(pedestal.getTileEntityClass(),pedestal.getRegistryName().toString());
@@ -22,12 +25,14 @@ public class ModBlocks {
     public static void registerItemBlocks(IForgeRegistry<Item> registry) {
         registry.registerAll(
                 oreCopper.createItemBlock(),
+                blockCopper.createItemBlock(),
                 pedestal.createItemBlock()
         );
     }
 
     public static void registerModels() {
         oreCopper.registerItemModel(Item.getItemFromBlock(oreCopper));
+        blockCopper.registerItemModel(Item.getItemFromBlock(blockCopper));
         pedestal.registerItemModel(Item.getItemFromBlock(pedestal));
     }
 
